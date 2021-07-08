d14d078fcae8b7367c566d1b63d20c206f376bf5
jonn <jm@memorici.de>
2021-07-08 16:23:37 +0100

    Problem: There is no way to keep server's state
    Solution:
    
     - Use atomically reference counted (Arc) mutex
     - Demonstrate its usage under server's closure by using `move`
       instruction

--- src/scratch_manpages.rs
+++ src/scratch_manpages.rs
@@ -1,7 +1,13 @@
 crate::entry_point!("manpages/http", http);
 
+use std::sync::Arc;
+use std::sync::Mutex;
+use std::thread;
+
+use chrono::prelude::*;
 use http::header;
 use simple_server::{Method, Server, StatusCode};
+use std::collections::BTreeMap;
 
 #[allow(dead_code)]
 pub fn int_div_round_up_i128(dividend: i128, divisor: i128) -> i128 {
@@ -33,7 +39,6 @@ pub fn diagonal_encode(x: &str) -> u32 {
         let charpos = alpha.find(c).unwrap_or(8) as u32;
         let triple_id = int_div_round_up_u32(charpos + 1, 3);
         let mul: u32 = 10_u32.pow(i as u32);
-        print!("({},{},{}) ", charpos, triple_id, mul);
         res += mul * (triple_id % 10);
     }
     res
@@ -43,26 +48,35 @@ pub fn stov(s: &str) -> Vec<u8> {
     s.as_bytes().to_vec()
 }
 
+type State = BTreeMap<NaiveDateTime, String>;
+
 fn http() {
     eprintln!("Hello from scratch");
 
-    let host = "127.0.0.1";
-    let port_string = format!("{}", diagonal_encode("icfp"));
-    let port = port_string.as_str();
-
-    let server = Server::new(|req, mut resp| {
-        eprintln!("Req {} {}", req.method(), req.uri());
+    let state = Arc::new(Mutex::new(State::new()));
+    let server = Server::new(move |req, mut resp| {
+        {
+            let state_ref = state.clone();
+            let mut state_contents = state_ref.lock().unwrap();
+            state_contents.insert(Utc::now().naive_utc(), "four oh four".to_string());
+            eprintln!("Req {} {} {:#?}", req.method(), req.uri(), *state_contents);
+        }
         resp.header(header::CONTENT_TYPE, "application/json".as_bytes());
         match (req.method(), req.uri().path()) {
-            (&Method::GET, "/hello") => Ok(resp.body(stov(r#"{"greeting": "hello, world!"}"#))?),
+            (&Method::GET, "/hello") => Ok(resp.body(stov(r#"{"greeting": "добрый вечер!"}"#))?),
             (_, _) => {
                 resp.header(header::CONTENT_TYPE, "text/html; charset=utf8".as_bytes());
                 resp.status(StatusCode::NOT_FOUND);
-                Ok(resp.body(stov("<h1>🤷 ЧОЧ</h1><p>Unicode (UTF-8, really) is fine.</p>"))?)
+                Ok(resp.body(stov(
+                    "<h1>🤷 ЧОЧ</h1><p>Unicode (UTF-8, really) is fine.</p>",
+                ))?)
             }
         }
     });
 
+    let host = "127.0.0.1";
+    let port_string = format!("{}", diagonal_encode("icfp"));
+    let port = port_string.as_str();
     eprintln!("Serving at http://{}:{} ...", host, port);
     server.listen(host, port);
 }
