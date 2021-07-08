55c105a55c5159e03d58d56b0ff65a0dc029ea30
jonn <jm@memorici.de>
2021-07-08 23:33:17 +0100

    Problem: No way to see timers
    Solution:
     - Add dashboard/timer endpoint
     - Notice that we shoudl URLdecode strings gotten via GET

--- src/scratch_manpages.rs
+++ src/scratch_manpages.rs
@@ -54,11 +54,34 @@ pub fn qstom(query_string: &str) -> HashMap<String, String> {
     let mut res = HashMap::new();
     for kv_str in kvs {
         let mut kv = kv_str.split('=');
-        let k = kv.next().unwrap().to_string();
-        let v = kv.next().unwrap().to_string();
+        let k = urldecode(kv.next().unwrap().to_string());
+        let v = urldecode(kv.next().unwrap().to_string());
         res.insert(k, v);
     }
     res
+}
+
+// https://docs.rs/urldecode/0.1.1/src/urldecode/lib.rs.html#1-21 don't at me
+pub fn urldecode(url: String) -> String {
+    let mut decoded = String::from("");
+    let mut skip = 0;
+    for i in 0..url.len() {
+        if skip != 0 {
+            skip -= 1;
+            continue;
+        }
+        let c: char = url.chars().nth(i).unwrap();
+        if c == '%' {
+            let left = url.chars().nth(i + 1).unwrap();
+            let right = url.chars().nth(i + 2).unwrap();
+            let byte = u8::from_str_radix(&format!("{}{}", left, right), 16).unwrap();
+            decoded += &(byte as char).to_string();
+            skip = 2;
+        } else {
+            decoded += &c.to_string();
+        }
+    }
+    decoded
 }
 
 #[allow(clippy::or_fun_call)]
@@ -77,8 +100,8 @@ pub fn with_q_map_mk_naivedatetime(q_string_map: HashMap<String, String>) -> Nai
     let month = if month > 0 { month } else { defaults.month() };
     let day = *q_map.get("d").unwrap_or(&default_day);
     let day = if day > 0 { day } else { default_day };
-    let hour = *q_map.get("H").unwrap_or(&defaults.month());
-    let min = *q_map.get("M").unwrap_or(&defaults.month());
+    let hour = *q_map.get("H").unwrap_or(&defaults.hour());
+    let min = *q_map.get("M").unwrap_or(&defaults.minute());
     let sec = 0;
     NaiveDate::from_ymd(year, month, day).and_hms(hour, min, sec)
 }
@@ -107,6 +130,18 @@ fn http() {
             // Observe how we aren't locked into this pattern of just matching the whole path.
             // We can achieve greater flexibility here by splitting, parsing prefix ["dashboard", "timer"] and then matching over the verb!
             // I can't be bothered now, too much stuff to figure out, sorry.
+            (&Method::GET, "/dashboard/timer") => {
+                let state_ref = state.clone();
+                let state_contents = &*state_ref.lock().unwrap();
+                if let Ok(serialized) = serde_json::to_string(&state_contents) {
+                    resp.header(header::CONTENT_TYPE, "application/json".as_bytes());
+                    Ok(resp.body(stov(&serialized))?)
+                } else {
+                    resp.header(header::CONTENT_TYPE, "text/html; charset=utf8".as_bytes());
+                    resp.status(StatusCode::INTERNAL_SERVER_ERROR);
+                    Ok(resp.body(stov("<h1>🤷 502</h1><p>D̾ͮ̎ͪ͏̰͖̳̣ą͙̖̘̩̱͙͂t̻̩͙̹̳̳̼̂ͭͧ̈́͡aͫ͏̯͇̠̲͔ ̧̗̳̜̥͌ͩ͗i̙̮͗͑́s̐̑̀̈́͏̬̙̪͍̭̱̙ ̰͇̦̗͂ͮ̍͡ͅc̛̫̪͙̣͔̙͉̣̓ǒ̄҉̥̙̟͕̝r̛̙̫̹͕͒r͕̻̫̼̘̻̂͌ͤ̎͜u̢̞͙͕͐̚ṕ̷̩͕̗̗̞̠̒̌̋t̲̜̺̭͖̝̅ͥ͘ͅe̵̮͖͈̝͔̥͎̎͆ͦ̒ͅd̷͉̙̰͖͉͙̐ͫ</p>"))?)
+                }
+            }
             (&Method::GET, "/dashboard/timer/insert") => {
                 if let Some(qs) = req.uri().query() {
                     let q_map = qstom(qs);
@@ -171,5 +206,13 @@ mod tests {
         assert_eq!(int_div_round_up_i32(1, 3), 1);
         assert_eq!(int_div_round_up_i32(0, 3), 0);
         assert_eq!(int_div_round_up_i32(-1, 3), 0);
+    }
+
+    #[test]
+    fn urldecode_isnt_completely_broken() {
+        assert_eq!(
+            "https://github.com/ /",
+            urldecode(String::from("https%3A%2F%2Fgithub.com/%20/"))
+        );
     }
 }
