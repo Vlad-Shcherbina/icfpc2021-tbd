950f2c7496e36dbfc830bae4d7d8ebbaaf37de26
Sergey Matveev <drw@technocore.net>
2021-07-09 16:21:05 +0300

    add the problem scraper script

---
+++ scripts/get_problem.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Usage:
+# scripts/get_problem.sh {first..last}
+
+project_root="$(dirname $0 )/../"
+
+for n in "$@"
+do
+	wget https://poses.live/problems/$n/download -O $project_root/data/problems/$n.problem
+done
