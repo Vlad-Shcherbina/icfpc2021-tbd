f2089e0d8769205b9b4ae49a2ab73a93a7fbb255
jonn <jm@memorici.de>
2021-07-09 02:40:13 +0100

    Problem: It is unclear how to make clients
    Solution:
    
     - Implement a demo-client for timer application

---
+++ public/get_fonts.sh
@@ -0,0 +1,4 @@
+mkdir ./public/fonts
+wget https://doma.2038.io/fonts/semibold.woff2 -O ./public/fonts/bold.woff2
+wget https://doma.2038.io/fonts/mono-regular.woff2 -O ./public/fonts/monospace.woff2
+wget https://doma.2038.io/fonts/regular.woff2 -O ./public/fonts/regular.woff2
