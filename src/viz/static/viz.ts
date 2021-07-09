9dd45c515dcc9b427749dc6a9d8588e092b443a9
caudate_julie <kyra-mensk@yandex.ru>
2021-07-09 18:38:42 +0300

    viz select and deselect figure vertices

--- src/viz/static/viz.ts
+++ src/viz/static/viz.ts
@@ -53,9 +53,15 @@ function get_frame(h: hole, f: figure) : Frame {
     return frame;
 }
 
-function rescale(p: number[], frame: Frame): number[] {
-    let nx = Math.floor(width / (frame.max_x - frame.min_x) * (p[0] - frame.min_x));
-    let ny = Math.floor(height / (frame.max_y - frame.min_y) * (p[1] - frame.min_y));
+function grid_to_screen(p: number[], frame: Frame): number[] {
+    let nx = Math.floor(width / (frame.max_x - frame.min_x) * (p[0] - frame.min_x)) + 0.5;
+    let ny = Math.floor(height / (frame.max_y - frame.min_y) * (p[1] - frame.min_y)) + 0.5;
+    return [nx, ny];
+}
+
+function screen_to_grid(p: number[], frame: Frame): number[] {
+    let nx = Math.floor((frame.max_x - frame.min_x) / width * (p[0] - 0.5) + frame.min_x);
+    let ny = Math.floor((frame.max_y - frame.min_y) / height * (p[1] - 0.5) + frame.min_y);
     return [nx, ny];
 }
 
@@ -66,29 +72,31 @@ function draw_hole(h: hole, frame: Frame) {
     ctx.strokeStyle = "#777777";
     ctx.lineWidth = 2;
     ctx.beginPath();
-    let p = rescale(h.hole[h.hole.length - 1], frame)
+    let p = grid_to_screen(h.hole[h.hole.length - 1], frame)
     ctx.moveTo(p[0], p[1]);
     for (let i = 0; i < h.hole.length; i++) {
-        let p = rescale(h.hole[i], frame);
+        let p = grid_to_screen(h.hole[i], frame);
         ctx.lineTo(p[0], p[1]);
     }
     ctx.stroke();
 }
 
-function draw_init_human(f: figure, frame: Frame) {
-    canvas_h_init.width = canvas_h_init.width;
-    // TODO: scale
-    let ctx = ctx_h_init;
-    ctx.strokeStyle = "#FF9B9B";
-    ctx.lineWidth = 1;
+function draw_human(f: figure, frame: Frame, ctx: CanvasRenderingContext2D, color: string, w: number) {
+    ctx.strokeStyle = color;
+    ctx.lineWidth = w;
     for (let i = 0; i < f.edges.length; i++) {
         ctx.beginPath();
-        let p1 = rescale(f.vertices[f.edges[i][0]], frame)
-        let p2 = rescale(f.vertices[f.edges[i][1]], frame)
+        let p1 = grid_to_screen(f.vertices[f.edges[i][0]], frame)
+        let p2 = grid_to_screen(f.vertices[f.edges[i][1]], frame)
         ctx.moveTo(p1[0], p1[1]);
         ctx.lineTo(p2[0], p2[1]);
         ctx.stroke();
     }
+}
+
+function draw_init_human(f: figure, frame: Frame) {
+    canvas_h_init.width = canvas_h_init.width;
+    draw_human(f, frame, ctx_h_init, "#FF9B9B", 1);
 }
 
 function draw_grid(frame: Frame) {
@@ -96,12 +104,60 @@ function draw_grid(frame: Frame) {
     ctx.fillStyle = "#BBBBBB";
     for (let x = frame.min_x; x < frame.max_x; x++) {
         for (let y = frame.min_y; y < frame.max_y; y++) {
-            let p = rescale([x, y], frame);
+            let p = grid_to_screen([x, y], frame);
             ctx.beginPath();
             ctx.arc(p[0], p[1], 1, 0, 2 * Math.PI);
             ctx.fill();
         }
     }
+}
+
+function mouse_coords_to_canvas(e: MouseEvent): number[] {
+    let r = canvas_human.getBoundingClientRect();
+    return [e.x - r.left, e.y - r.top];
+}
+
+function closest(e: MouseEvent, f: figure, frame: Frame, delta: number): number | null {
+    let mp = mouse_coords_to_canvas(e);
+    for (let i = 0; i < f.vertices.length; i++) {
+        let p = grid_to_screen(f.vertices[i], frame);
+        if ((p[0] - mp[0]) * (p[0] - mp[0]) + (p[1] - mp[1]) * (p[1] - mp[1]) < delta * delta) {
+            return i;
+        }
+    }
+    return null;
+}
+
+let marked: number[] = []
+
+function mark_point(e: MouseEvent, f: figure, frame: Frame, delta: number) {
+    let p = closest(e, f, frame, delta);
+    if (p == null) return;
+    let i = marked.indexOf(p);
+    if (i == -1) {
+        marked.push(p);
+    }
+    else {
+        marked.splice(i, 1);
+    }
+    redraw_figure(f, frame, marked);
+}
+
+function draw_marked_points(m: number[], f: figure, frame: Frame) {
+    let ctx = ctx_human;
+    ctx.fillStyle = "#0026FF";
+    for (let m of marked) {
+        let p = grid_to_screen(f.vertices[m], frame);
+        ctx.beginPath();
+        ctx.arc(p[0], p[1], 4, 0, 2 * Math.PI);
+        ctx.fill();
+    }
+}
+
+function redraw_figure(f: figure, frame: Frame, marked: number[]) {
+    canvas_human.width = canvas_human.width;
+    draw_human(f, frame, ctx_human, "#A83F3F", 2);
+    draw_marked_points(marked, f, frame);
 }
 
 let h: hole = JSON.parse('{ "hole": [[55, 80], [65, 95], [95, 95], [35, 5], [5, 5],[35, 50], [5, 95], [35, 95], [45, 80]] }');
@@ -111,4 +167,4 @@ draw_hole(h, frame);
 draw_grid(frame);
 draw_init_human(f, frame);
 
-canvas_human.onmousedown = () => { draw_hole(h, frame); };
+canvas_human.onmousedown = (e: MouseEvent) => { mark_point(e, f, frame, 10); };
