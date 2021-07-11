e0f62b0ab3e5c826a295f1d9fe48b5752f308266
Pavel.Sergeev <pavel.sergeev@jetbrains.com>
2021-07-11 17:51:27 +0300

    randomize bruteforce; fix clippy warns

--- src/bruteforce.rs
+++ src/bruteforce.rs
@@ -1,25 +1,30 @@
 use crate::shake::ShakeRequest;
 use crate::prelude::{Problem, Pt};
-use std::collections::{HashMap, HashSet};
 use crate::graph::bfs;
 use crate::mango::available_positions;
+use rand::prelude::SliceRandom;
 
-fn selected_idxs(selected: &Vec<bool>) -> Vec<usize> {
-    selected.iter().enumerate()
-        .filter(|(_, b)| **b)
-        .map(|(idx, _)| idx)
-        .collect()
-}
+// fn selected_idxs(selected: &Vec<bool>) -> Vec<usize> {
+//     selected.iter().enumerate()
+//         .filter(|(_, b)| **b)
+//         .map(|(idx, _)| idx)
+//         .collect()
+// }
 
-fn go(problem: &Problem, free_nodes: &mut Vec<usize>, places: &mut Vec<Option<Pt>>) -> Option<Vec<Pt>> {
+fn go(problem: &Problem,
+      free_nodes: &mut Vec<usize>,
+      places: &mut Vec<Option<Pt>>) -> Option<Vec<Pt>> {
     // in bfs order
     // place vertex in any availiable position according already placed
     // until all vertex will be placed
     // check
+    let rng = &mut rand::thread_rng();
 
     while !free_nodes.is_empty() {
         let current_id = free_nodes.pop().unwrap();
         let mut availiable_positions = available_positions(problem, places, current_id);
+
+        availiable_positions.shuffle(rng);
         // eprintln!("availiable_positions: {:?}", availiable_positions);
         for pt in availiable_positions.drain(0..) {
             places[current_id] = Some(pt);
@@ -34,9 +39,9 @@ fn go(problem: &Problem, free_nodes: &mut Vec<usize>, places: &mut Vec<Option<Pt
     // eprintln!("places: {:?}", places);
 
     if places.iter().all(|pt| pt.is_some()) {
-        return Some(places.iter().map(|pt| pt.unwrap()).collect());
+        Some(places.iter().map(|pt| pt.unwrap()).collect())
     } else {
-        return None;
+        None
     }
 }
 
@@ -46,7 +51,7 @@ fn brutforce_with(problem: &Problem, v_id: usize, pt: Pt) -> Option<Vec<Pt>> {
     order.pop();
     let mut places: Vec<Option<Pt>> = vec![None; problem.figure.vertices.len()];
     places[v_id] = Some(pt);
-    go(&problem, &mut order, &mut places)
+    go(problem, &mut order, &mut places)
 }
 
 pub fn brutforce(r: &ShakeRequest) -> Vec<Pt> {
@@ -58,6 +63,5 @@ pub fn brutforce(r: &ShakeRequest) -> Vec<Pt> {
             }
         }
     }
-    return r.vertices.clone();
-
+    r.vertices.clone()
 }
