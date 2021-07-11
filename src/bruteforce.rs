5057ae579a98f09c280dc69fc774621e779a8cc2
Pavel.Sergeev <pavel.sergeev@jetbrains.com>
2021-07-11 20:11:40 +0300

    brutforce sort by candidate positions by dislikes

--- src/bruteforce.rs
+++ src/bruteforce.rs
@@ -3,13 +3,8 @@ use crate::prelude::{Problem, Pt};
 use crate::graph::bfs;
 use crate::mango::available_positions;
 use rand::prelude::SliceRandom;
+use crate::checker::get_dislikes;
 
-// fn selected_idxs(selected: &Vec<bool>) -> Vec<usize> {
-//     selected.iter().enumerate()
-//         .filter(|(_, b)| **b)
-//         .map(|(idx, _)| idx)
-//         .collect()
-// }
 
 fn go(problem: &Problem,
       free_nodes: &mut Vec<usize>,
@@ -18,15 +13,21 @@ fn go(problem: &Problem,
     // place vertex in any availiable position according already placed
     // until all vertex will be placed
     // check
-    let rng = &mut rand::thread_rng();
 
     while !free_nodes.is_empty() {
         let current_id = free_nodes.pop().unwrap();
-        let mut availiable_positions = available_positions(problem, places, current_id);
 
-        availiable_positions.shuffle(rng);
+        let mut vertices: Vec<_> = places.iter().filter_map(|pt| *pt).collect();
+        let mut available_positions = available_positions(problem, places, current_id);
+        available_positions.sort_by_cached_key(|pt| {
+            vertices.push(*pt);
+            let dislikes = get_dislikes(problem, &vertices);
+            vertices.pop();
+            dislikes
+        });
+
         // eprintln!("availiable_positions: {:?}", availiable_positions);
-        for pt in availiable_positions.drain(0..) {
+        for pt in available_positions.drain(0..) {
             places[current_id] = Some(pt);
             let result = go(problem, free_nodes, places);
             if result.is_some() {
@@ -55,8 +56,13 @@ fn brutforce_with(problem: &Problem, v_id: usize, pt: Pt) -> Option<Vec<Pt>> {
 }
 
 pub fn brutforce(r: &ShakeRequest) -> Vec<Pt> {
-    for v_id in 0..r.vertices.len() {
-        for (h_id, pt) in r.problem.hole.iter().enumerate() {
+    let rng = &mut rand::thread_rng();
+    let mut v_ids: Vec<_> = (0..r.vertices.len()).into_iter().collect();
+    v_ids.shuffle(rng);
+    let mut h_ids = r.problem.hole.clone();
+    h_ids.shuffle(rng);
+    for v_id in v_ids {
+        for (h_id, pt) in h_ids.iter().enumerate() {
             eprintln!("Trying: v_id {:?} h_id {:?}", v_id, h_id);
             if let Some(result) = brutforce_with(&r.problem, v_id, *pt) {
                 return result;
